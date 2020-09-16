d6740ff913726a6e36432fe5f09e69971c156082
Miri <Miri@JOEY>
2020-09-16 19:30:44 +0300

    initial commit - first screen (without cards or players)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Grid} from "@material-ui/core";
+import { sizing } from '@material-ui/system';
+const cardDeckSize = 27
+const cardsInHand = 6
+class Game extends React.Component
+{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: Array(cardDeckSize).fill(Card)
+        };
+    }
+    renderCard(i)
+    {
+        /*let BG = './cardsImg/' + i + '.jpg';
+         import(BG).then(image=>{this.cards[i].image = image});
+         console.log(BG);
+         return(
+             this.state.cards[i]
+         );*/
+        let BG = './cardsImg/' + i + '.jpg';
+        return(
+            <button className={"card"} style={{backgroundImage: `url(${BG})`}}></button>
+        );
+    }
+    render() {
+        return (
+            <Grid container alignItems={"stretch"} justify={"space-between"} direction={"column"} className={"big-grid"}>
+                <Grid item container alignItems={"center"} direction={"column"}>
+                    <Grid item><h1>You are the storyteller</h1></Grid>
+                    <Grid item><h2>pick a card</h2></Grid>
+                </Grid>
+                <Grid item>
+                    <p>players</p>
+                </Grid>
+                <Grid item container direction={"row"} justify={"space-around"} alignItems={"center"}>
+                    <Grid item>{this.renderCard(0)}</Grid>
+                    <Grid item>{this.renderCard(1)}</Grid>
+                    <Grid item>{this.renderCard(2)}</Grid>
+                    <Grid item>{this.renderCard(3)}</Grid>
+                    <Grid item>{this.renderCard(4)}</Grid>
+                    <Grid item>{this.renderCard(5)}</Grid>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+class Card extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: props.id,
+          image: props.image,
+          inUse: false,
+          used: false
+        };
+    }
+
 }
 
-export default App;
+export default Game;
