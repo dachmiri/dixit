1850db03909032389ffe0db2946eca29f7923d16
Miri <Miri@JOEY>
2020-09-28 20:39:08 +0300

    players box & cards

--- src/App.js
+++ src/App.js
@@ -1,42 +1,102 @@
 import React from 'react';
 import './App.css';
 import {Grid} from "@material-ui/core";
-import { sizing } from '@material-ui/system';
+import Button from '@material-ui/core/Button';
+
 const cardDeckSize = 27
 const cardsInHand = 6
+const playersInGame = 3 //Will come from server later
 class Game extends React.Component
 {
 
     constructor(props) {
         super(props);
         this.state = {
-            cards: Array(cardDeckSize).fill(Card)
+            cards: Array(cardDeckSize),
+            players: Array(playersInGame)
         };
+
+        this.initPlayers()
+        this.initCardDeck()
+
+        console.log("players:");
+        console.log(this.state.players)
+    }
+    initPlayers() {
+        for (let index = 0; index < playersInGame; index++)
+        {
+            var tempNames = ["Anna", "Noa", "Maya"];
+            var tempIcons = ["icons/pig.png", "icons/bunny.png", "icons/fox.png"];
+            const p = new Player({
+                id: index,
+                pName: tempNames[index],
+                icon: tempIcons[index]
+            });
+            this.state.players.push(p)
+        }
+    }
+    initCardDeck() {
+        var tempCards = []
+        for (let index = 0; index < cardDeckSize; index++)
+        {
+            const c = <Card id={index} image={"temp"} />
+            tempCards.push(c);
+        }
+        this.state.cards = tempCards;
     }
     renderCard(i)
     {
-        /*let BG = './cardsImg/' + i + '.jpg';
-         import(BG).then(image=>{this.cards[i].image = image});
-         console.log(BG);
-         return(
-             this.state.cards[i]
-         );*/
-        let BG = './cardsImg/' + i + '.jpg';
+        let cardImg = 'cardsImg/' + i + '.jpg';
         return(
-            <button className={"card"} style={{backgroundImage: `url(${BG})`}}></button>
+            <Button className={"card"} style={{backgroundImage: `url(${cardImg})`, backgroundSize: "cover", backgroundRepeat: "no-repeat"}}> </Button>
         );
     }
     render() {
         return (
-            <Grid container alignItems={"stretch"} justify={"space-between"} direction={"column"} className={"big-grid"}>
+            <Grid container alignItems={"stretch"} justify={"space-between"} direction={"column"} className={"big-grid"} spacing={5}>
                 <Grid item container alignItems={"center"} direction={"column"}>
                     <Grid item><h1>You are the storyteller</h1></Grid>
                     <Grid item><h2>pick a card</h2></Grid>
                 </Grid>
-                <Grid item>
-                    <p>players</p>
+                <Grid item container className={"players"}>
+                    <Grid item container direction={"row"} spacing={3} alignItems={"center"}>
+                        <Grid item>
+                            <Button
+                                style={{backgroundImage: `url(${this.state.players[3].state.icon})`,
+                                        backgroundSize: "cover",
+                                        minWidth: "100px",
+                                        minHeight: "100px"}}>
+                            </Button>
+                        </Grid>
+                        <Grid item><p>{this.state.players[3].state.pName}</p></Grid>
+                        <Grid item><p>{this.state.players[3].state.score}</p></Grid>
+                    </Grid>
+                    <Grid item container direction={"row"} spacing={3} alignItems={"center"}>
+                        <Grid item>
+                            <Button
+                                style={{backgroundImage: `url(${this.state.players[4].state.icon})`,
+                                       backgroundSize: "cover",
+                                       minWidth: "100px",
+                                       minHeight: "100px"}}>
+                            </Button>
+                        </Grid>
+                        <Grid item><p>{this.state.players[4].state.pName}</p> </Grid>
+                        <Grid item><p>{this.state.players[4].state.score}</p> </Grid>
+                    </Grid>
+                    <Grid item container direction={"row"} spacing={3} alignItems={"center"}>
+                        <Grid item>
+                            <Button
+                                style={{backgroundImage: `url(${this.state.players[5].state.icon})`,
+                                        backgroundSize: "cover",
+                                        minWidth: "100px",
+                                        minHeight: "100px"}}>
+                            </Button>
+                        </Grid>
+                        <Grid item><p>{this.state.players[5].state.pName}</p> </Grid>
+                        <Grid item><p>{this.state.players[5].state.score}</p> </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item container direction={"row"} justify={"space-around"} alignItems={"center"}>
+                <Grid item container direction={"row"} justify={"space-around"} alignItems={"center"} >
                     <Grid item>{this.renderCard(0)}</Grid>
                     <Grid item>{this.renderCard(1)}</Grid>
                     <Grid item>{this.renderCard(2)}</Grid>
@@ -55,11 +115,21 @@ class Card extends React.Component
         this.state = {
           id: props.id,
           image: props.image,
-          inUse: false,
-          used: false
         };
     }
 
+}
+class Player extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: props.id,
+          pName: props.pName,
+          score: 0,
+          icon: props.icon,
+        };
+    }
 }
 
 export default Game;
